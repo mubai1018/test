efb9edbca58c479de3f151f9d2571355f6e70f3f
mubai1018 <2453262964@qq.com>
2024-05-23 19:27:29 +0800

    新增测试文件

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+
+    public static void main(String[] args) {
+        //写一个冒泡排序的程序
+        int[] arr = {5, 2, 8, 3, 9, 1, 7, 4, 6};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
